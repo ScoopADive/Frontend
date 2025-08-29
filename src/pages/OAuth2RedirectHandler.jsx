b43c57d2eebf2deb565fa23f740dfd11e716760a
junnie082 <junnie082@gmail.com>
2025-08-29 10:39:11 +0900

    fix: 구글 로그인 관련 오류 수정

--- src/pages/OAuth2RedirectHandler.jsx
+++ src/pages/OAuth2RedirectHandler.jsx
@@ -1,7 +1,7 @@
-import { useEffect } from "react";
-import { useNavigate, useLocation } from "react-router-dom";
-import authService from "../services/authService";
-import useUserStore from "../store/userStore";
+import { useEffect } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+import authService from '../services/authService';
+import useUserStore from '../store/userStore';
 
 const OAuth2RedirectHandler = () => {
   const location = useLocation();
@@ -10,25 +10,25 @@ const OAuth2RedirectHandler = () => {
 
   useEffect(() => {
     const params = new URLSearchParams(location.search);
-    const token = params.get("token");
-    const email = params.get("email");
-    const name = params.get("name");
-    const id = params.get("id"); // id 받아오기
+    const token = params.get('token');
+    const email = params.get('email');
+    const name = params.get('name');
+    const id = params.get('id'); // id 받아오기
 
     if (token && email && id) {
       // 토큰 및 유저 정보 저장
-      localStorage.setItem("access_token", token);
-      localStorage.setItem("email", email);
-      localStorage.setItem("name", name);
-      localStorage.setItem("id", id); // 추가
+      localStorage.setItem('access_token', token);
+      localStorage.setItem('email', email);
+      localStorage.setItem('name', name);
+      localStorage.setItem('id', id); // 추가
 
       // Zustand에 사용자 정보 반영
       setUser({ id, email, name });
 
-      navigate("/mypage"); // 로그인 후 마이페이지로 이동
+      navigate('/mypage'); // 로그인 후 마이페이지로 이동
     } else {
-      alert("로그인 처리에 실패했습니다.");
-      navigate("/signin");
+      alert('로그인 처리에 실패했습니다.');
+      navigate('/signin');
     }
   }, [location, navigate, setUser]);
 
